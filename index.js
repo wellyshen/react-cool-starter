a4c131ee5cf20635b86064d77a261798520a5902
WellyShen <wellyshen@kkbox.com>
2016-08-30 21:31:54 +0800

    Code rearrange

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('babel-core/register'); // Enable runtime transpilation to use ES6/7 in node
 
-const WebpackIsomorphicTools = require('webpack-isomorphic-tools');
-// This should be the same with webpack context
-const dirRoot = require('path').join(__dirname, '.');
-
 // Setup global variables for use
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DEV__ = process.env.NODE_ENV !== 'production';
+
+const WebpackIsomorphicTools = require('webpack-isomorphic-tools');
+// This should be the same with webpack context
+const dirRoot = require('path').join(__dirname, '.');
 
 // Settings of webpack-isomorphic-tools
 // eslint-disable-next-line max-len
