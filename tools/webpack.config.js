4f8596e7091ce2b770bab315fa872d38ba1687c0
WellyShen <wellyshen@kkbox.com>
2016-09-04 19:36:34 +0800

    Add babel-ployfill for who want to use

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -79,6 +79,7 @@ function getEntry() {
       app: './src/client.js',
       // Register vendors here
       vendor: [
+        'babel-polyfill',
         'react', 'react-dom', 'react-addons-shallow-compare',
         'redux', 'react-redux',
         'redux-thunk',
