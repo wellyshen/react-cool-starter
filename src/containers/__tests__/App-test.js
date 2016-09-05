4e3cd55e4ccc332fc0d1e35835b85dca7e10da99
WellyShen <wellyshen@kkbox.com>
2016-09-05 21:43:38 +0800

    update

--- src/containers/__tests__/App-test.js
+++ src/containers/__tests__/App-test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { expect, assert } from 'chai';
 
 import App from '../App';
 
--- tools/webpack/config.test.js
+++ tools/webpack/config.test.js
@@ -29,7 +29,7 @@ module.exports = function (CSSModules) {  // eslint-disable-line func-names
       cheerio: 'window',
       'react/addons': true,
       'react/lib/ExecutionEnvironment': true,
-      'react/lib/ReactContext': 'window',
+      'react/lib/ReactContext': true,
     },
     resolve: {
       extensions: ['', '.js', '.jsx', '.json'],
