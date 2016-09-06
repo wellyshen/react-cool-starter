86b7c6210db3d395efbc7dd432ad7cf9e3501128
WellyShen <wellyshen@kkbox.com>
2016-09-07 00:18:33 +0800

    update

--- src/containers/__tests__/Home-test.js
+++ src/containers/__tests__/Home-test.js
@@ -4,21 +4,15 @@ import { shallow } from 'enzyme'; // eslint-disable-line import/no-extraneous-de
 import { Map } from 'immutable';
 import Home from '../Home';
 
-function setup() {
-  return {
-    fakeStore: {
-      default: () => {},
-      subscribe: () => {},
-      dispatch: () => {},
-      getState: () => {},
-    },
-  };
-}
+const fakeStore = {
+  default: () => {},
+  subscribe: () => {},
+  dispatch: () => {},
+  getState: () => {},
+};
 
 describe('<Home />', () => {
-  const { fakeStore } = setup();
-
-  it('render the loading indicator when data is invalid', () => {
+  it('renders the loading indicator when data is invalid', () => {
     const fakeData = Map({ // eslint-disable-line new-cap
       readyState: 'USERS_INVALID',
       list: null,
@@ -32,7 +26,7 @@ describe('<Home />', () => {
     expect(wrapper.text().indexOf('Loading...')).to.equal(-1);
   });
 
-  it('render the loading indicator when data is fetching', () => {
+  it('renders the loading indicator when data is fetching', () => {
     const fakeData = Map({ // eslint-disable-line new-cap
       readyState: 'USERS_FETCHING',
       list: null,
