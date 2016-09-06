620c81dd45fefdbb208140b53107c72eeda2fdab
WellyShen <wellyshen@kkbox.com>
2016-09-06 14:45:04 +0800

    Update

--- src/containers/__tests__/App-test.js
+++ src/containers/__tests__/App-test.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import config from '../../config';
 import App from '../App';
 
 describe('<App />', () => {
-  it('should display a logo', () => {
-    const wrapper = shallow(<App />);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders a logo', () => {
     const image = wrapper.find('img');
 
     expect(image).to.have.length(1);
-    assert.equal(image.prop('src'), require('../App/logo.svg'));
+    expect(image.prop('src')).to.equal(require('../App/logo.svg'));
+  });
+
+  it('renders a title', () => {
+    const title = wrapper.find('h1');
+
+    expect(title).to.have.length(1);
+    expect(title.text()).to.equal(config.app.title);
+  });
+
+  it('renders the children content', () => {
+    should.exist(wrapper.props('children'));
   });
 });
