80b5a590f2b79cfc9c64bd01c45e2e52f71b150d
WellyShen <wellyshen@kkbox.com>
2016-09-06 23:37:31 +0800

    update

--- src/containers/__tests__/Home-test.js
+++ src/containers/__tests__/Home-test.js
@@ -1,35 +1,41 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { shallow } from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
-import { Map, fromJS } from 'immutable';
-import Home from '../Home';
+import { Map } from 'immutable';
+import { Home } from '../Home';
 
 describe('<Home />', () => {
-  let fakeStore;
-  let fakeData;
-  let wrapper;
+  // let fakeStore;
+  // let fakeData;
+  // let wrapper;
 
-  beforeEach(() => {
-    fakeStore = {
-      default: () => {},
-      subscribe: () => {},
-      dispatch: () => {},
-      getState: () => {},
-    };
+  // beforeEach(() => {
+  //   fakeStore = {
+  //     default: () => {},
+  //     subscribe: () => {},
+  //     dispatch: () => {},
+  //     getState: () => {},
+  //   };
 
-    fakeData = Map({ // eslint-disable-line new-cap
-      readyState: 'USERS_FETCHED',
-      list: fromJS([{ name: 'Welly' }]),
+  //   fakeData = Map({ // eslint-disable-line new-cap
+  //     readyState: 'USERS_FETCHED',
+  //     list: fromJS([{ name: 'Welly' }]),
+  //   });
+
+  //   wrapper = shallow(
+  //     <Provider store={fakeStore}>
+  //       <Home users={fakeData} />
+  //     </Provider>
+  //   );
+  // });
+
+  it('render the loading indicator when its loading', () => {
+    const fakeData = Map({ // eslint-disable-line new-cap
+      readyState: 'USERS_INVALID',
+      list: null,
     });
+    const wrapper = shallow(<Home users={fakeData} dispatch={() => {}} />);
 
-    wrapper = shallow(
-      <Provider store={fakeStore}>
-        <Home users={fakeData} />
-      </Provider>
-    );
-  });
-
-  it('renders self', () => {
-    expect(wrapper.find(Home)).to.have.length(1);
+    expect(wrapper.find('p').text()).to.equal('Loading...');
   });
 });
