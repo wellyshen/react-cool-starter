89b53b1760a38f17acca30e868e2b4c4b0964bb3
WellyShen <wellyshen@kkbox.com>
2016-09-07 16:20:01 +0800

    Update

--- src/containers/__tests__/Home-test.js
+++ src/containers/__tests__/Home-test.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { mount } from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
 import { fromJS } from 'immutable';
 import Home from '../Home';
 
 describe('<Home />', () => {
-  it('renders it', () => {
-    const mockData = fromJS({
-      readyState: 'USERS_INVALID',
-      list: null,
-    });
-    const wrapper = mount(<Home users={mockData} />);
+  it('renders <Home />', () => {
+    const props = {
+      users: fromJS({
+        readyState: 'USERS_INVALID',
+        list: null,
+      }),
+    };
+
+    const wrapper = mount(
+      <Provider>
+        <Home ...props />
+      </Provider>
+    );
 
     // console.log(wrapper);
   });
