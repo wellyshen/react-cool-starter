dc0225da609e840411f49f6b3ac02cf28586694a
WellyShen <wellyshen@kkbox.com>
2016-09-07 17:31:00 +0800

    Update

--- src/containers/__tests__/Home-test.js
+++ src/containers/__tests__/Home-test.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
-import { Map, fromJS } from 'immutable';
+import { fromJS } from 'immutable';
 import Home from '../Home';
 
 const storeFake = state => ({
   default: () => {},
   subscribe: () => {},
   dispatch: () => {},
-  getState: () => ({ ...state }),
+  getState: () => fromJS({ ...state }),
 });
 
 describe('<Home />', () => {
   it('renders <Home />', () => {
     const store = storeFake({
-      users: () => Map({  // eslint-disable-line new-cap
+      users: {
         readyState: 'USERS_FETCHED',
-        list: fromJS([{ name: 'Welly' }]),
-      }),
+        list: [{ name: 'Welly' }],
+      },
     });
 
     const wrapper = mount(
