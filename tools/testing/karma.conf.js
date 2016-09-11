5033524f76209ab4880a81fbee27deb84e335d22
WellyShen <wellyshen@kkbox.com>
2016-09-11 19:45:22 +0800

    Refine karma config

--- tools/testing/karma.conf.js
+++ tools/testing/karma.conf.js
@@ -5,14 +5,23 @@ const webpackConfig = require('../webpack');
 module.exports = function (config) {
   config.set({
     browsers: ['PhantomJS'],
+
+    autoWatch: false,
+
     singleRun: true,
+
     frameworks: ['mocha'],
+
     files: ['./test-bundler.js'],
+
     preprocessors: {
       './test-bundler.js': ['webpack', 'sourcemap'],
     },
+
     reporters: ['mocha'],
+
     webpack: webpackConfig,
+
     // Make Webpack bundle generation quiet
     webpackMiddleware: {
       noInfo: true,
