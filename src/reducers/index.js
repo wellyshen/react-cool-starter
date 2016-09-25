4c6cc3f671bea33105e29f12a8cf4a244b76560e
WellyShen <wellyshen@kkbox.com>
2016-09-25 16:45:20 +0800

    Update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,6 +7,8 @@ import routing from './routing';
 export default function createReducer(asyncReducers) {
   return combineReducers({
     routing,
+    // Define the inital state for the async reducers
+    // otherwise you will get the warning of Redux
     users: (state = Map({})) => state,
     anUser: (state = Map({})) => state,
     ...asyncReducers,
