75e05ab5f52dcf9fadf24eafceb290a94eba8eee
WellyShen <wellyshen@kkbox.com>
2016-09-27 17:41:12 +0800

    Correct code coverage

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
+/* istanbul ignore if */
 if (process.env.NODE_ENV !== 'production') {
   module.exports = require('./default');
+/* istanbul ignore else */
 } else {
   module.exports = require('./prod');
 }
