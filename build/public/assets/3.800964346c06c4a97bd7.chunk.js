9f1fcbaeae601733eb34068f46d813a771be7837
WellyShen <wellyshen@kkbox.com>
2016-10-20 22:57:06 +0800

    update

--- build/public/assets/3.800964346c06c4a97bd7.chunk.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([3],{467:function(e,n,t){"use strict";var a=t(0),o=t.n(a),c=t(85),u=t.n(c),l=t(497),s=t.n(l);n.default=function(){return o.a.createElement("div",{className:s.a.NotFound},o.a.createElement(u.a,{title:"Oops"}),o.a.createElement("p",null,"Oops, Page was not found!"))}},497:function(e,n){e.exports={NotFound:"_2SLcp860poLJ8lM5NKezSY"}}});
