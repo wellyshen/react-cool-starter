16d2fbd283818761a61aa9ad359a917827c36ac1
WellyShen <wellyshen@kkbox.com>
2016-11-27 15:04:51 +0800

    Remove OccurrenceOrderPlugin

--- tools/webpack/webpack.client.babel.js
+++ tools/webpack/webpack.client.babel.js
@@ -82,8 +82,7 @@ const getPlugins = () => {
         compress: { screw_ie8: true, warnings: false },
         output: { comments: false },
         sourceMap: false,
-      }),
-      new webpack.optimize.OccurrenceOrderPlugin(true)
+      })
     );
   }
 
