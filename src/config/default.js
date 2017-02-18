7864051b1482823180fdff9944c0b758bfaff198
WellyShen <wellyshen@kkbox.com>
2017-02-18 17:10:35 +0800

    Code refine

--- src/config/default.js
+++ src/config/default.js
@@ -6,7 +6,10 @@ module.exports = {
     title: 'React Cool Starter',
     titleTemplate: 'React Cool Starter - %s',
     meta: [
-      { name: 'description', content: 'The best react universal starter boilerplate in the world.' },
+      {
+        name: 'description',
+        content: 'The best react universal starter boilerplate in the world.',
+      },
     ],
   },
 };
