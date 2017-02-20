e44f9325adecdaf9be57ac8435bc5f2de774e078
WellyShen <wellyshen@kkbox.com>
2017-02-20 15:49:17 +0800

    Code refine

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ global.__DISABLE_SSR__ = false;   // Disable server side render here
 global.__DEV__ = process.env.NODE_ENV !== 'production';
 
 // This should be the same with webpack context
-// eslint-disable-next-line import/newline-after-import
 const dirRoot = require('path').join(process.cwd());
-const serverPath = __DEV__ ? './src/server' : './build/server';
 
 // Settings of webpack-isomorphic-tools
 global.webpackIsomorphicTools = new WebpackIsomorphicTools(require('./tools/webpack/WIT.config'))
   .server(dirRoot, () => {
-    require(serverPath);  // eslint-disable-line import/no-dynamic-require
+    if (__DEV__) {
+      require('./src/server');
+    } else {
+      require('./build/server');
+    }
   });
