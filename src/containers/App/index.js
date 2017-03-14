7948ef7be5bf53efa080d521e3f37dafc7987850
WellyShen <wellyshen@kkbox.com>
2017-03-14 19:00:17 +0800

    update

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,13 +3,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Helmet from 'react-helmet';
-// import _ from 'lodash';
+import _ from 'lodash';
 
 import config from '../../config';
-// import routes from '../../routes';
-import Home from '../Home';
-import UserInfo from '../UserInfo';
-import NotFound from '../NotFound';
+import routes from '../../routes';
+// import Home from '../Home';
+// import UserInfo from '../UserInfo';
+// import NotFound from '../NotFound';
 import '../../theme/normalize.css';
 import styles from './styles.scss';
 
@@ -22,22 +22,18 @@ export default () => (
     </div>
     <hr />
     <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/UserInfo/:id" component={UserInfo} />
-      <Route path="*" component={NotFound} />
+      {
+        routes.map(route => (
+          <Route
+            key={_.uniqueId()}
+            path={route.path}
+            render={props => (
+              // pass the sub-routes down to keep nesting
+              <route.component {...props} routes={route.routes} />
+            )}
+          />
+        ))
+      }
     </Switch>
   </div>
 );
-
-/* {
-  routes.map(route => (
-    <Route
-      key={_.uniqueId()}
-      path={route.path}
-      render={props => (
-        // pass the sub-routes down to keep nesting
-        <route.component {...props} routes={route.routes} />
-      )}
-    />
-  ))
-} */
