8460e97bb7a075493d752fdaf122d04c6da96928
WellyShen <wellyshen@kkbox.com>
2017-03-14 22:13:20 +0800

    update

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,40 +1,25 @@
 /* @flow */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Helmet from 'react-helmet';
-import _ from 'lodash';
 
 import config from '../../config';
+import { routeWithSubRoutes } from '../../utils/helpers';
 import routes from '../../routes';
 import '../../theme/normalize.css';
 import styles from './styles.scss';
 
-export default () => {
-  // When sub routes are added to any route it'll work
-  const RouteWithSubRoutes = (route, index) => (
-    <Route
-      key={_.uniqueId()}
-      path={route.path}
-      exact={index === 0 || false}
-      render={props => (
-        // Pass the sub-routes down to keep nesting
-        <route.component {...props} routes={route.routes} />
-      )}
-    />
-  );
-
-  return (
-    <div className={styles.App}>
-      <Helmet {...config.app} />
-      <div className={styles.header}>
-        <img src={require('./assets/logo.svg')} alt="Logo" role="presentation" />
-        <h1>{config.app.title}</h1>
-      </div>
-      <hr />
-      <Switch>
-        {routes.map((route, index) => RouteWithSubRoutes(route, index))}
-      </Switch>
+export default () => (
+  <div className={styles.App}>
+    <Helmet {...config.app} />
+    <div className={styles.header}>
+      <img src={require('./assets/logo.svg')} alt="Logo" role="presentation" />
+      <h1>{config.app.title}</h1>
     </div>
-  );
-};
+    <hr />
+    <Switch>
+      {routes.map((route, index) => routeWithSubRoutes(route, index))}
+    </Switch>
+  </div>
+);
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,23 @@
 /* @flow */
 
+import React, { Element } from 'react';
+import { Route } from 'react-router-dom';
+import _ from 'lodash';
+
+// When sub routes are added to any route it'll work
+export const routeWithSubRoutes = (route: Object, index: number): Element<any> => (
+  <Route
+    key={_.uniqueId()}
+    path={route.path}
+    exact={index === 0 || false}
+    render={props => (
+      // Pass the sub-routes down to keep nesting
+      <route.component {...props} routes={route.routes} />
+    )}
+  />
+);
+
 // The fake store creator for testing Components
-// eslint-disable-next-line import/prefer-default-export
 export function storeFake(state: Object): Object {
   return {
     default: () => {},
