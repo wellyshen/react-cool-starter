8a2b57e0084af5ab74251bdcaba1a0269b5dae72
WellyShen <wellyshen@kkbox.com>
2017-03-15 20:33:16 +0800

    Update

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,31 @@
-/* @flow */
-
+import { asyncComponent } from './utils/helpers';
 import type { Dispatch } from './types';
 import Home from './containers/Home';
 import UserInfo from './containers/UserInfo';
-import NotFound from './containers/NotFound';
+// import NotFound from './containers/NotFound';
 
 export default [
   {
     path: '/',
     exact: true,
-    component: Home,
+    component: asyncComponent(() => Promise.all([
+      import('./containers/Home'),
+    ]).then(([Component]) => Component.default)),
     loadData: (dispatch: Dispatch) => Home.fetchData(dispatch),
   },
   {
     path: '/UserInfo/:id',
     exact: false,
-    component: UserInfo,
+    component: asyncComponent(() => Promise.all([
+      import('./containers/UserInfo'),
+    ]).then(([Component]) => Component.default)),
     loadData: (dispatch: Dispatch, params: Object) => UserInfo.fetchData(dispatch, params),
   },
   {
     path: '*',
     exact: false,
-    component: NotFound,
+    component: asyncComponent(() => Promise.all([
+      import('./containers/NotFound'),
+    ]).then(([Component]) => Component.default)),
   },
 ];
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17,6 +17,33 @@ export const routeWithSubRoutes = (route: Object): Element<any> => (
   />
 );
 
+// getComponent is a function that returns a promise for a component
+// It will not be called until the first mount
+export const asyncComponent = (getComponent: () => Promise<any>) =>
+  class AsyncComponent extends React.Component {
+    static Component = null;
+
+    state = { Component: AsyncComponent.Component };
+
+    componentDidMount() {
+      if (this.state.Component) return;
+
+      getComponent().then((Component) => {
+        AsyncComponent.Component = Component;
+
+        this.setState({ Component });
+      });
+    }
+
+    render() {
+      const { Component } = this.state;
+
+      if (Component) return <Component {...this.props} />;
+
+      return null;
+    }
+  };
+
 // The fake store creator for testing Components
 export function storeFake(state: Object): Object {
   return {
