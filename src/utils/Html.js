08350fdf04b21f161aaa3a5ed2d2843ff141d45e
WellyShen <wellyshen@kkbox.com>
2017-03-17 11:59:21 +0800

    Update

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -9,86 +9,84 @@ import type { Store } from '../types';
 
 type Props = { store: Store, htmlContent?: string };
 
-const Html = ({ store, htmlContent }: Props) => {
-  const head = Helmet.rewind();
-  const attrs = head.htmlAttributes.toComponent();
-  const { lang, ...rest } = attrs || {};
-  const assets = webpackIsomorphicTools.assets();
+const head = Helmet.rewind();
+const attrs = head.htmlAttributes.toComponent();
+const { lang, ...rest } = attrs || {};
+const assets = webpackIsomorphicTools.assets();
 
-  return (
-    <html {...rest} lang={lang || 'en'}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+const Html = ({ store, htmlContent }: Props) => (
+  <html {...rest} lang={lang || 'en'}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+      <link rel="shortcut icon" href="/favicon.ico" />
 
-        {head.base.toComponent()}
-        {head.title.toComponent()}
-        {head.meta.toComponent()}
-        {head.link.toComponent()}
+      {head.base.toComponent()}
+      {head.title.toComponent()}
+      {head.meta.toComponent()}
+      {head.link.toComponent()}
 
-        {/* Styles will be presented in production with webpack extract text plugin */}
-        {_.keys(assets.styles).map(style =>
-          <link
-            key={_.uniqueId()}
-            href={assets.styles[style]}
-            media="screen, projection"
-            rel="stylesheet"
-            type="text/css"
-          />,
-        )}
-        {/* Styles will be presented in development mode */}
-        {/* I put all of the styles here to smoothen the flick */}
-        {
-          _.keys(assets.styles).length === 0 ?
-            <style
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{ __html:
-                // $FlowFixMe: It's not an issue
-                require('../theme/normalize.css')._style +
-                // $FlowFixMe: It's not an issue
-                require('../containers/App/styles.scss')._style +
-                // $FlowFixMe: It's not an issue
-                require('../containers/Home/styles.scss')._style +
-                // $FlowFixMe: It's not an issue
-                require('../containers/UserInfo/styles.scss')._style +
-                // $FlowFixMe: It's not an issue
-                require('../containers/NotFound/styles.scss')._style +
-                // $FlowFixMe: It's not an issue
-                require('../components/UserList/styles.scss')._style +
-                // $FlowFixMe: It's not an issue
-                require('../components/UserCard/styles.scss')._style,
-              }}
-            />
-            : null
-        }
-      </head>
-      <body>
-        <div
-          id="react-view"
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: htmlContent || '' }}
-        />
+      {/* Styles will be presented in production with webpack extract text plugin */}
+      {_.keys(assets.styles).map(style =>
+        <link
+          key={_.uniqueId()}
+          href={assets.styles[style]}
+          media="screen, projection"
+          rel="stylesheet"
+          type="text/css"
+        />,
+      )}
+      {/* Styles will be presented in development mode */}
+      {/* I put all of the styles here to smoothen the flick */}
+      {
+        _.keys(assets.styles).length === 0 ?
+          <style
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html:
+              // $FlowFixMe: It's not an issue
+              require('../theme/normalize.css')._style +
+              // $FlowFixMe: It's not an issue
+              require('../containers/App/styles.scss')._style +
+              // $FlowFixMe: It's not an issue
+              require('../containers/Home/styles.scss')._style +
+              // $FlowFixMe: It's not an issue
+              require('../containers/UserInfo/styles.scss')._style +
+              // $FlowFixMe: It's not an issue
+              require('../containers/NotFound/styles.scss')._style +
+              // $FlowFixMe: It's not an issue
+              require('../components/UserList/styles.scss')._style +
+              // $FlowFixMe: It's not an issue
+              require('../components/UserCard/styles.scss')._style,
+            }}
+          />
+          : null
+      }
+    </head>
+    <body>
+      <div
+        id="react-view"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: htmlContent || '' }}
+      />
 
-        <script
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{
-            __html: store && `window.__INITIAL_STATE__=${serialize(store.getState())};`,
-          }}
-        />
-        {
-          // Reverse the order of scripts for accessing vendor.js first
-          _.keys(assets.javascript).reverse().map(script =>
-            <script key={_.uniqueId()} src={assets.javascript[script]} />,
-          )
-        }
-        {head.script.toComponent()}
-      </body>
-    </html>
-  );
-};
+      <script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: store && `window.__INITIAL_STATE__=${serialize(store.getState())};`,
+        }}
+      />
+      {
+        // Reverse the order of scripts for accessing vendor.js first
+        _.keys(assets.javascript).reverse().map(script =>
+          <script key={_.uniqueId()} src={assets.javascript[script]} />,
+        )
+      }
+      {head.script.toComponent()}
+    </body>
+  </html>
+);
 
 Html.defaultProps = { htmlContent: '' };
 
