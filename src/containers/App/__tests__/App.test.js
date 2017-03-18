48fcc68a27cbcfd7deb9957edc44cb80bbcbfef4
WellyShen <wellyshen@kkbox.com>
2017-03-18 16:52:44 +0800

    update

--- src/containers/App/__tests__/App.test.js
+++ src/containers/App/__tests__/App.test.js
@@ -1,28 +1,40 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { shallow } from 'enzyme';
 
 import App from '../index';
 import config from '../../../config';
 
 describe('<App />', () => {
-  const routes = [{
+  const route = [{
     path: '/',
     exact: true,
-    component: () => (<div><p>Mock route</p></div>),
+    component: () => <div><p>Hi, it rocks!</p></div>,
   }];
-  const wrapper = shallow(<App routes={routes} />);
+  const wrapper = shallow(<App routes={route} />);
 
   test('renders a logo', () => {
     const image = wrapper.find('img');
 
     expect(image).toHaveLength(1);
-    expect(image.prop('src')).toEqual(require('../assets/logo.svg'));
+    expect(image.prop('src')).toBe(require('../assets/logo.svg'));
   });
 
   test('renders a title', () => {
     const title = wrapper.find('h1');
 
     expect(title).toHaveLength(1);
-    expect(title.text()).toEqual(config.app.title);
+    expect(title.text()).toBe(config.app.title);
+  });
+
+  test('renders routes', () => {
+    const RRSwitch = wrapper.find(Switch);
+    const RRRoute = RRSwitch.find(Route);
+
+    expect(RRSwitch).toHaveLength(1);
+    expect(RRRoute).toHaveLength(1);
+    expect(RRRoute.prop('path')).toBe('/');
+    expect(RRRoute.prop('exact')).toBeTruthy();
+    // expect(RRRoute.prop('render')).toBe(route[0].component);
   });
 });
--- src/containers/NotFound/__tests__/NotFound.test.js
+++ src/containers/NotFound/__tests__/NotFound.test.js
@@ -7,6 +7,6 @@ describe('<NotFound />', () => {
   test('renders 404 error message', () => {
     const wrapper = shallow(<NotFound />);
 
-    expect(wrapper.find('p').text()).toEqual('Oops, Page was not found!');
+    expect(wrapper.find('p').text()).toBe('Oops, Page was not found!');
   });
 });
