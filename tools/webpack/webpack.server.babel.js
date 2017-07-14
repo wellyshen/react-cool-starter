86b7de68fed6d2c2bd85899a146dedffc222a529
WellyShen <wellyshen@kkbox.com>
2017-07-14 17:51:51 +0800

    Use Baili to minify javascript with es6

--- tools/webpack/webpack.server.babel.js
+++ tools/webpack/webpack.server.babel.js
@@ -36,6 +36,7 @@ module.exports = {
         options: {
           babelrc: false,
           presets: [['es2015', { modules: false }], 'react', 'stage-0'],
+          env: { production: { presets: ['babili'] } },
         },
       },
       {
