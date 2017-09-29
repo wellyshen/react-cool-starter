9d5ab5232192de85fe8a3cb5457fc1b4f0ddf704
Welly Shen <hivoid19@gmail.com>
2017-09-29 23:02:19 +0800

    Upgrade to react v16

--- tools/openBrowser/index.js
+++ tools/openBrowser/index.js
@@ -46,11 +46,7 @@ function executeNodeScript(scriptPath, url) {
   child.on('close', (code) => {
     if (code !== 0) {
       console.log();
-      console.log(
-        chalk.red(
-          'The script specified as BROWSER environment variable failed.',
-        ),
-      );
+      console.log(chalk.red('The script specified as BROWSER environment variable failed.'));
       console.log(`${chalk.cyan(scriptPath)} exited with code ${code}.`);
       console.log();
 
@@ -67,8 +63,7 @@ function startBrowserProcess(browser, url) {
   // Chrome with AppleScript. This lets us reuse an
   // existing tab when possible instead of creating a new one.
   const shouldTryOpenChromeWithAppleScript =
-    process.platform === 'darwin' &&
-    (typeof browser !== 'string' || browser === OSX_CHROME);
+    process.platform === 'darwin' && (typeof browser !== 'string' || browser === OSX_CHROME);
 
   if (shouldTryOpenChromeWithAppleScript) {
     try {
