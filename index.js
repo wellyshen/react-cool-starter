b001f83dd3d4532f1e2b9370a663291c82ec3379
Welly Shen <hivoid19@gmail.com>
2017-10-07 15:41:05 +0800

    Fix index.js run error

--- index.js
+++ index.js
@@ -15,8 +15,5 @@ global.__DEV__ = process.env.NODE_ENV !== 'production';
 const dirRoot = require('path').join(process.cwd());
 
 // Settings of webpack-isomorphic-tools
-global.webpackIsomorphicTools = new WebpackIsomorphicTools(
-  require('./tools/webpack/WIT.config'),
-).server(dirRoot, () => {
-  require('./src/server');
-});
+global.webpackIsomorphicTools =
+  new WebpackIsomorphicTools(require('./tools/webpack/WIT.config')).server(dirRoot, () => require('./src/server'));
