ea83917c8766cccc74a3d9090892ceebb7e3cd7a
Welly Shen <hivoid19@gmail.com>
2017-10-07 15:59:53 +0800

    Fix the mismatch error of css module of App.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -29,7 +29,7 @@ const App = (): Element<'div'> => {
   return (
     <div className={styles.App}>
       <Helmet {...config.app} />
-      <div className={styles.header}>
+      <div>
         <img src={require('./assets/logo.svg')} alt="Logo" role="presentation" />
         <h1>{config.app.title}</h1>
       </div>
