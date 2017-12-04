3e2e9f332d5825a3697aae3e046f3eb7fd62d87e
Welly Shen <hivoid19@gmail.com>
2017-12-04 22:54:52 +0800

    Code refactor

--- src/types/index.js
+++ src/types/index.js
@@ -3,36 +3,36 @@
 import type { Store as ReduxStore } from 'redux';
 
 export type Home = {
-  readyStatus: string,
-  err: any,
-  list: Array<Object>,
+  +readyStatus: string,
+  +err: any,
+  +list: Array<Object>,
 };
 
 export type UserInfo = {
-  [userId: string]: {
-    readyStatus: string,
-    err: any,
-    info: Object,
+  +[userId: string]: {
+    +readyStatus: string,
+    +err: any,
+    +info: Object,
   },
 };
 
 export type Reducer = {
-  home: Home,
-  userInfo: UserInfo,
-  router: any,
+  +home: Home,
+  +userInfo: UserInfo,
+  +router: any,
 };
 
 export type Action =
-  { type: 'USERS_REQUESTING' } |
-  { type: 'USERS_SUCCESS', data: Array<Object> } |
-  { type: 'USERS_FAILURE', err: any } |
-  { type: 'USER_REQUESTING', userId: string } |
-  { type: 'USER_SUCCESS', userId: string, data: Object } |
-  { type: 'USER_FAILURE', userId: string, err: any };
+  | { type: 'USERS_REQUESTING' }
+  | { type: 'USERS_SUCCESS', data: Array<Object> }
+  | { type: 'USERS_FAILURE', err: any }
+  | { type: 'USER_REQUESTING', userId: string }
+  | { type: 'USER_SUCCESS', userId: string, data: Object }
+  | { type: 'USER_FAILURE', userId: string, err: any };
 
 export type Store = ReduxStore<Reducer, Action>;
 // eslint-disable-next-line no-use-before-define
 export type Dispatch = (action: Action | ThunkAction | PromiseAction | Array<Action>) => any;
-export type GetState = () => Object;
+export type GetState = () => Reducer;
 export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
 export type PromiseAction = Promise<Action>;
