bc2a108a892623fdeccb5fcfe67bc4bf00423921
Welly Shen <hivoid19@gmail.com>
2018-02-04 15:27:16 +0800

    Remove App

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,7 @@
 /* @flow */
 
-import App from './App';
 import HomePage from './Home';
 import UserInfoPage from './UserInfo';
 import NotFoundPage from './NotFound';
 
-export { App, HomePage, UserInfoPage, NotFoundPage };
+export { HomePage, UserInfoPage, NotFoundPage };
