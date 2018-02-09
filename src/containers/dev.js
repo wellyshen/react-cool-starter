a54638ac43f53391f477bd2632a3859119d4f03e
Welly Shen <hivoid19@gmail.com>
2018-02-09 18:01:21 +0800

    Enable hot reloading for development & Enable type checking for "./src/config" files

---
+++ src/containers/dev.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+// Using sync components on developmemt to support hot reloading
+
+import HomePage from './Home/Home';
+import UserInfoPage from './UserInfo/UserInfo';
+import NotFoundPage from './NotFound/NotFound';
+
+export { HomePage, UserInfoPage, NotFoundPage };
