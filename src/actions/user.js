4016232f31aad181aae4eb907f72fbbe357b700b
alireza valizade <alirezavalizade0@gmail.com>
2018-02-22 18:06:34 +0330

    remove unused console

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,14 +1,11 @@
 /* @flow */
 
 /* istanbul ignore next */
-export const fetchUser = userId => {
-  console.log(userId);
-  return {
-    types: 'USER',
-    url: `/users/${userId}`,
-    method: 'get',
-    userId
-  };
-};
+export const fetchUser = userId => ({
+  types: 'USER',
+  url: `/users/${userId}`,
+  method: 'get',
+  userId
+});
 
 export const fetchUserIfNeeded = params => fetchUser(params);
