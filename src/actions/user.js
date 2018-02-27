55db348e2b4fdd5173193367f1165fd7fc254a60
Welly Shen <hivoid19@gmail.com>
2018-02-28 01:14:07 +0800

    Fixing wrong middleware path & Upgrade dependencies & Fixing flow errors

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,11 +1,11 @@
 /* @flow */
 
 /* istanbul ignore next */
-export const fetchUser = userId => ({
+export const fetchUser = (userId: string) => ({
   types: 'USER',
   url: `/users/${userId}`,
   method: 'get',
   userId
 });
 
-export const fetchUserIfNeeded = params => fetchUser(params);
+export const fetchUserIfNeeded = (params: string) => fetchUser(params);
