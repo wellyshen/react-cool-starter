1c6630da98b31cd30144e7c93f3787b352f94094
Welly Shen <hivoid19@gmail.com>
2018-04-29 23:40:34 +0800

    Fix travis CI build error

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /* @flow */
 
 import path from 'path';
@@ -21,7 +22,7 @@ import configureStore from './helpers/configureStore';
 import renderHtml from './helpers/renderHtml';
 import routes from './routes';
 // $FlowFixMe: isn't an issue
-import assets from '../public/webpack-assets'; // eslint-disable-line import/extensions
+import assets from '../public/webpack-assets.json';
 import { port, host } from './config';
 
 const app = express();
