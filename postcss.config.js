17d610680bc34a6f2cf88a82574bc949bbd1befb
Welly Shen <hivoid19@gmail.com>
2018-04-29 22:42:16 +0800

    Remove un-necessary import

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
 /* Set your postcss-loader configuration here */
 
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
   plugins: [
     // Add "-ms-" prefixes for Grid Layout
-    autoprefixer({ grid: true })
+    require('autoprefixer')({ grid: true })
   ]
 };
