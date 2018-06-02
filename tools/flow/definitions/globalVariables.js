ed4bd30334eb68d38e38d06d73223fba78d2afe8
Welly Shen <wellyshen@kkstream.com.tw>
2018-06-02 15:45:37 +0800

    Remove un-used global variable & Upgrade dependencies

--- tools/flow/definitions/globalVariables.js
+++ tools/flow/definitions/globalVariables.js
@@ -4,7 +4,6 @@
 declare var __CLIENT__: bool;
 declare var __SERVER__: bool;
 declare var __DEV__: bool;
-declare var webpackIsomorphicTools: any;
 declare var should: any;
 declare var expect: any;
 declare var assert: any;
