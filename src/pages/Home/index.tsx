4d6e5aa6447019f83cb8f17ef41c9e5fa6ba99be
Welly Shen <hivoid19@gmail.com>
2019-09-26 18:27:18 +0800

    Code refactor

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import loadable from '@loadable/component';
+
 import { Loading, ErrorBoundary } from '../../components';
 
 const Home = loadable(() => import('./Home'), {
