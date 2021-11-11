f9e9822d08a1e7e5ed6d65d6f9f22532b36e0bb7
Welly <hivoid19@gmail.com>
2021-11-11 22:31:30 +0800

    Update Info.tsx

--- src/components/Info/__tests__/Info.tsx
+++ src/components/Info/__tests__/Info.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
-import Info from "../index";
+import Info from "..";
 
 describe("<Info />", () => {
   it("renders", () => {
