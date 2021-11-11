47ba71831a49d2d5785fa886f2defca0ddb5423a
Welly <hivoid19@gmail.com>
2021-11-11 22:31:43 +0800

    Update Loading.tsx

--- src/components/Loading/__tests__/Loading.tsx
+++ src/components/Loading/__tests__/Loading.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
-import Loading from "../index";
+import Loading from "..";
 
 describe("<Loading />", () => {
   it("renders", () => {
